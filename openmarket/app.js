518d57b24da549624df0e9dbf486324bbfa13a4b
Jimmy <jimmy.delacruz@student.uclouvain.be>
2019-09-28 09:36:01 +0200

    fix css in ejs

--- openmarket/app.js
+++ openmarket/app.js
@@ -7,6 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var requestsRouter = require('./routes/requests');
+var shopRouter = require('./routes/shop');
 
 var app = express();
 
@@ -23,6 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/requests', requestsRouter);
+app.use('/shop',shopRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ openmarket/routes/shop.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var router = express.Router();
+var requests_controller = require('../controllers/requests_controller');
+
+/* GET shop*/
+router.get('/', function(req, res, next) {
+  var requests = requests_controller.get_all_requests();
+  requests.then(function(data){ console.log(data); res.render('index',{ 'products': data.RowDataPacket }); }).catch(function(err){console.log(err)});
+});
+
+module.exports = router;
