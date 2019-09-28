b10a19aa284e4b2b10d94863dcd13d26a3240bb9
Jonathan Samelson <jonathan@samelson.info>
2019-09-28 04:34:00 +0200

    Added requests routes + added description

---
+++ openmarket/controllers/requests_controller.js
@@ -0,0 +1,34 @@
+var mysql = require('mysql');
+const fs = require('fs');
+ 
+var condb = mysql.createConnection({
+    host: "localhost",
+    user: "user_db",
+    password: "user_pass",
+    database: "openmarket"
+});
+
+exports.get_all_requests = () => {
+    var sql = 'SELECT * FROM requests';
+    condb.query(sql, [], function (err, result) {
+        if (err) throw err;
+        console.log(result);
+    });
+};
+
+exports.get_one_requests = (id) => {
+    var sql = 'SELECT * FROM requests WHERE request_id = ?';
+    condb.query(sql, [id], function (err, result) {
+        if (err) throw err;
+        console.log(result);
+    });
+};
+
+exports.create_request = (data) => {
+    console.log(data);
+    var sql = 'INSERT INTO requests (name, peremption_date, customer_id, price, type_id, description) VALUES (?, ?, ?, ?, ?, ?)';
+    condb.query(sql, [data.name, data.peremption_date, data.customer_id, data.price, data.type_id, data.description], function (err, result) {
+        if (err) throw err;
+        console.log(result);
+    });
+}
